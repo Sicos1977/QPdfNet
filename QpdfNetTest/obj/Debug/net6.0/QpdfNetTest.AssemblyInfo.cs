c6069f31c1fc4f1dd6a91f17976dd4aa257ba298
Kees van Spelde <keesvanspelde@hotmail.com>
2024-05-17 20:00:17 +0200

    Updat nuget packages

--- QpdfNetTest/obj/Debug/net6.0/QpdfNetTest.AssemblyInfo.cs
+++ QpdfNetTest/obj/Debug/net6.0/QpdfNetTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QpdfNetTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+502d284e5c68c3ef12412cf0af2973fc3485548c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fdacee4ae84968b526e235c543a5c5bc74e5657")]
 [assembly: System.Reflection.AssemblyProductAttribute("QpdfNetTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QpdfNetTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
